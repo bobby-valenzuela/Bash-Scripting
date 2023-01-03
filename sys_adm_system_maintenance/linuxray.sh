025cd68b483cc9b59048d64165dcd0b57cbbc43a
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2023-01-03 12:39:28 -0800

    Update linuxray.sh

--- sys_adm_system_maintenance/linuxray.sh
+++ sys_adm_system_maintenance/linuxray.sh
@@ -23,3 +23,38 @@ ps auxf | sort -nr -k 3 | head -5
 ## Update cache and upgrade binaries
 # sudo apt update
 
+
+
+
+#######  TODO ########
+
+
+# Dangling link cleanup
+# old file archiver
+# integrity checker
+# kill processes pointing to deleted files (lsof) 
+# critical disk usage (show top three large files)
+# systemd-analyze blame (see which process tkes the longest to start pm boot)
+# Check for any soft links not using absolute paths pointing to destination
+# Check for any links
+# load average (lscpu to see num cores)
+# Security checks
+  #  Prone to fork bomb?
+  # mysql --version to see if mysql/maris db is enabled and port is open on 3306.
+  # check fstab for deice name instead of UUIDs
+  # Listening on standard SSH port
+  #  SSH see if root login is enabled
+  # SSH see is password auth is accepted
+  # use chage -l <username> to show last password change
+  # - last time fsck was run?
+  # w or who to see idle users for long time
+  # lastb to see number of failed login attempts for past day
+  # systemd mask to see if processes can start disabled processes?
+  # print this in or Banner?
+  # systemctl mask ctrl-alt-del.target (disable reboot hotley)
+  # verify SSH file/dir permissions
+  # git master/main preventkju
+  # vscode cleanup 
+  # John the ripper? 
+  # package is cleanup 
+  # c option too compress large text files (any greater than 100mb) 
