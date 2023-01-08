1c6488fa39d5459d0d2b8002bac3fb9bc025606e
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2023-01-08 05:28:25 -0800

    Update _snippets.sh

--- _snippets.sh
+++ _snippets.sh
@@ -28,3 +28,55 @@ even_or_odd()
     [[ ! $1 =~ $integertest ]] && echo "Not an integer!" && return 1
     [[ $(( $1 % 2 )) == 0 ]] && echo "Even" || echo "Odd"
 }
+
+
+
+# **********************************************************
+#           PRINTING AND FORMATTING
+# **********************************************************
+
+# Get count of characters wide the terminal is
+COLUMNS=$(tput cols)
+
+prHeader(){
+
+    for each in $(seq 1 $COLUMNS)
+
+    do
+
+    echo -n $1
+
+    done
+
+}
+
+prtxtCentre(){
+
+title=$1
+
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+
+}
+
+
+
+# print colored text
+print_colored()
+{
+
+    [[ -z "$1" ||  -z "$2" ]] && echo "Usage: print_colored <color> <text>" && exit 1
+
+    case "$1" in
+        "grey" | "GREY")        echo -e "\033[90m$2 \033[00m" ;;
+        "red" | "RED")          echo -e "\033[91m$2 \033[00m" ;;
+        "green" | "GREEN")      echo -e "\033[92m$2 \033[00m" ;;
+        "yellow" | "YELLOW")    echo -e "\033[93m$2 \033[00m" ;;
+        "blue" | "BLUE")        echo -e "\033[94m$2 \033[00m" ;;
+        "purple" | "PURPLE")    echo -e "\033[95m$2 \033[00m" ;;
+        "cyan" | "CYAN")        echo -e "\033[96m$2 \033[00m" ;;
+        "white" | "WHITE")      echo -e "\033[96m$2 \033[00m" ;;
+        *   )               echo -e "\033[96m$2 \033[00m" ;;
+    esac
+
+
+}
