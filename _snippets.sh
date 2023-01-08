d8808d1a94191e1039c64cd6cd3153fe03b993b4
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2023-01-08 05:47:39 -0800

    Update _snippets.sh

--- _snippets.sh
+++ _snippets.sh
@@ -65,18 +65,25 @@ print_colored()
 {
 
     [[ -z "$1" ||  -z "$2" ]] && echo "Usage: print_colored <color> <text>" && exit 1
+    
+    auto_print_newline=''
+    [[ "$3" == 'no' ]] && auto_print_newline='-n' 
 
     case "$1" in
-        "grey" | "GREY")        echo -e "\033[90m$2 \033[00m" ;;
-        "red" | "RED")          echo -e "\033[91m$2 \033[00m" ;;
-        "green" | "GREEN")      echo -e "\033[92m$2 \033[00m" ;;
-        "yellow" | "YELLOW")    echo -e "\033[93m$2 \033[00m" ;;
-        "blue" | "BLUE")        echo -e "\033[94m$2 \033[00m" ;;
-        "purple" | "PURPLE")    echo -e "\033[95m$2 \033[00m" ;;
-        "cyan" | "CYAN")        echo -e "\033[96m$2 \033[00m" ;;
-        "white" | "WHITE")      echo -e "\033[96m$2 \033[00m" ;;
-        *   )               echo -e "\033[96m$2 \033[00m" ;;
+        "grey" | "GREY")        echo -e ${auto_print_newline} "\033[90m$2 \033[00m" ;;
+        "red" | "RED")          echo -e ${auto_print_newline} "\033[91m$2 \033[00m" ;;
+        "green" | "GREEN")      echo -e ${auto_print_newline} "\033[92m$2 \033[00m" ;;
+        "yellow" | "YELLOW")    echo -e ${auto_print_newline} "\033[93m$2 \033[00m" ;;
+        "blue" | "BLUE")        echo -e ${auto_print_newline} "\033[94m$2 \033[00m" ;;
+        "purple" | "PURPLE")    echo -e ${auto_print_newline} "\033[95m$2 \033[00m" ;;
+        "cyan" | "CYAN")        echo -e ${auto_print_newline} "\033[96m$2 \033[00m" ;;
+        "white" | "WHITE")      echo -e ${auto_print_newline} "\033[96m$2 \033[00m" ;;
+        *   )               echo -e ${auto_print_newline} "\033[96m$2 \033[00m" ;;
     esac
 
 
 }
+
+# Examples
+# print_colored 'red' 'my message'
+# print_colored 'red' 'my message\n\n' 'no'
