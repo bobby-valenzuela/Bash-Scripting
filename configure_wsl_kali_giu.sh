a3067df621820162159da859e758333215df0931
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2023-01-24 22:23:38 -0800

    Update configure_wsl_kali_giu.sh

--- configure_wsl_kali_giu.sh
+++ configure_wsl_kali_giu.sh
@@ -1,13 +1,16 @@
-sudo apt update && sudo apt upgrade -u
+# Update/upgrade
 sudo apt update && sudo apt upgrade -y
+# Install top 10 kali tolls (could also use katiloon)
 sudo apt install kali-tools-top10 -y
+# Install Kali Desltop Manager (xfce) 
 sudo apt install kali-desktop-xfce -y
+# Install kex gui
 sudo apt install kali-win-kex -y
-
-
-# Start Kex (manually)
-# kex --win -s
 
 # Start Kex (automatically)
 # Windows Terminal - edit profile cmd (add kex --wtstart -s)
 # C:\Windows\system32\wsl.exe -d kali-linux kex --wtstart -s
+
+
+# Start Kex (manually)
+# kex --win -s
