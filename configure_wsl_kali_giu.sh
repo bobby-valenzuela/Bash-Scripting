2235c580ab4c86d48fb9fbe439ddbc9b84900489
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2023-01-25 09:15:30 -0800

    Update configure_wsl_kali_giu.sh

--- configure_wsl_kali_giu.sh
+++ configure_wsl_kali_giu.sh
@@ -1,7 +1,9 @@
 # Update/upgrade
 sudo apt update && sudo apt upgrade -y
 # Install top 10 kali tolls (could also use katiloon)
-sudo apt install kali-tools-top10 -y
+sudo apt install kali-tools-top10 -y # Add more as needed here: https://www.kali.org/tools/kali-meta/
+
+
 # Install Kali Desltop Manager (xfce) 
 sudo apt install kali-desktop-xfce -y
 # Install kex gui
@@ -12,8 +14,13 @@ sudo apt install kali-win-kex -y
 # C:\Windows\system32\wsl.exe -d kali-linux kex --wtstart -s
 
 
-# Start Kex (manually)
+# Start Kex (manually) - run in home dir
 # kex --win -s
+# or...
+# kex --win --wtstart
 
-# This makes sure to start both server and client
-# sudo kex --win --wtstart
+### TROUBLESHOOTING ###
+# kill like 7 sed
+# (If you are getting messages like "Actively refused" or "Trouble reading passwd file")
+# mount -o remount rw /tmp/.X11-unix
+# cd ; kex --win --wtstart
