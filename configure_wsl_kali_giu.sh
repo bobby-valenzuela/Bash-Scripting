e88937b1ad3cb4efda9483391d2fff88605d799b
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2023-01-24 22:20:18 -0800

    Create configure_wsl_kali_giu.sh

---
+++ configure_wsl_kali_giu.sh
@@ -0,0 +1,13 @@
+sudo apt update && sudo apt upgrade -u
+sudo apt update && sudo apt upgrade -y
+sudo apt install kali-tools-top10 -y
+sudo apt install kali-desktop-xfce -y
+sudo apt install kali-win-kex -y
+
+
+# Start Kex (manually)
+# kex --win -s
+
+# Start Kex (automatically)
+# Windows Terminal - edit profile cmd (add kex --wtstart -s)
+# C:\Windows\system32\wsl.exe -d kali-linux kex --wtstart -s
