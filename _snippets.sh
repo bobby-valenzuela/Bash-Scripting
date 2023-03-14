0ad3cb683b0a18429c8262cf9a0014e41b76e20a
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2023-03-14 15:34:21 -0700

    Added ANSI escape codes for prompt color

--- _snippets.sh
+++ _snippets.sh
@@ -65,6 +65,17 @@ using_netstat=$({ netstat --version &> /dev/null && echo 1 ; } || echo 0)
 #           PRINTING AND FORMATTING
 # **********************************************************
 
+# ___ANSI Escape Codes___
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+
 # Get count of characters wide the terminal is
 COLUMNS=$(tput cols)
 
